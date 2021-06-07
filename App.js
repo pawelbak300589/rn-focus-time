5a5e4589ab0d9b0cf9c3a93f72c877a012a62d5f
Pawel Bak <pawelbak300589@gmail.com>
2021-06-07 19:44:51 +0100

    Change way of exporting/importing focus container and add rounded button next to the input

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Focus } from './src/features/focus/Focus';
+import Focus from './src/features/focus/Focus';
 
 export default function App() {
   const [focusSubject, setFocusSubject] = useState(null);
--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
-export const Focus = () => {
+import RoundedButton from '../../components/RoundedButton';
+
+const Focus = () => {
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>What would you like to focus on?</Text>
         <View style={styles.inputContainer}>
-          <TextInput />
+          <TextInput style={styles.input} />
+          <RoundedButton size={50} title="+" />
         </View>
       </View>
     </View>
@@ -30,6 +33,13 @@ const styles = StyleSheet.create({
     fontSize: 24
   },
   inputContainer: {
+    flexDirection: 'row',
     paddingTop: 20
+  },
+  input: {
+    flex: 1,
+    marginRight: 20
   }
 });
+
+export default Focus;
