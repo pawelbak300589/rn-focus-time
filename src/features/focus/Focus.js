bcdd8df582e8936b089f39c8dbcc2d0c497a0b55
Pawel Bak <pawelbak300589@gmail.com>
2021-06-07 19:29:44 +0100

    Add input to the focus component

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 export const Focus = () => {
   return (
     <View style={styles.container}>
-      <Text>What would you like to focus on?</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What would you like to focus on?</Text>
+        <View style={styles.inputContainer}>
+          <TextInput />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 50
+    flex: 1
   },
+  titleContainer: {
+    flex: 0.5,
+    padding: 16,
+    justifyContent: 'center'
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24
+  },
+  inputContainer: {
+    paddingTop: 20
+  }
 });
