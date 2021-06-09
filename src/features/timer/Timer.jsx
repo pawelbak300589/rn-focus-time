5710d28696c3c1d5d7bb09f0ad43ca9687ef551a
Pawel Bak <pawelbak300589@gmail.com>
2021-06-09 22:33:58 +0100

    Keep app awake when timer starts

--- src/features/timer/Timer.jsx
+++ src/features/timer/Timer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { ProgressBar } from 'react-native-paper';
+import { useKeepAwake } from 'expo-keep-awake';
 
 import Countdown from '../../components/Countdown';
 import RoundedButton from '../../components/RoundedButton';
@@ -9,6 +10,8 @@ import { spacing } from '../../utils/sizes';
 import Timing from './Timing';
 
 const Timer = ({ focusSubject }) => {
+  useKeepAwake();
+
   const [minutes, setMinutes] = useState(0.1);
   const [isStarted, setIsStarted] = useState(false);
   const [progress, setProgress] = useState(1);
