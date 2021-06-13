eab73a14c52a51927e31f05999ad68c00c9e8a81
Pawel Bak <pawelbak300589@gmail.com>
2021-06-13 13:00:37 +0100

    Add storing and loading the focus history

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Focus from './src/features/focus/Focus';
 import FocusHistory from './src/features/focus/FocusHistory';
@@ -23,6 +24,34 @@ export default function App() {
   const onClear = () => {
     setFocusHistory([]);
   };
+
+  const saveFocusHistory = async () => {
+    try {
+      await AsyncStorage.setItem("focusHistory", JSON.stringify(focusHistory));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadFocusHistory = async () => {
+    try {
+      const history = await AsyncStorage.getItem("focusHistory");
+
+      if (history && JSON.parse(history).length) {
+        setFocusHistory(JSON.parse(history));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadFocusHistory();
+  }, []);
+
+  useEffect(() => {
+    saveFocusHistory();
+  }, [focusHistory]);
 
   return (
     <View style={styles.container}>
